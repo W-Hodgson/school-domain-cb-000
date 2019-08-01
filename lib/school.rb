4d0ce2a9cf0d2f4568a41e57150f931dc24cb5ef
Will Hodgson <will.hodgson@current-rms.com>
2019-08-01 12:31:01 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -5,11 +5,11 @@ class School
   def initialize(name)
     @name = name
     @roster = {}
-    @roster[grade] = []
+
   end
 
   def add_student(name, grade)
-
+    @roster[grade] = []
     @roster[grade] << name
   end
 end
